4f6eed56d75515e0d1727585a25b1c37de355a61
renshuai <renshuai@yuerushui.com>
2019-07-11 16:38:15 +0800

    客户端与服务端双向通信

---
+++ src/main/java/com/sren/netty/client/NettyClient.java
@@ -0,0 +1,9 @@
+package com.sren.netty.client;
+
+/**
+ * @author: renshuai
+ * @date: 2019/07/11 下午4:37
+ * @Description:
+ */
+public class NettyClient {
+}
---
+++ src/main/java/com/sren/netty/server/NettyServer.java
@@ -0,0 +1,9 @@
+package com.sren.netty.server;
+
+/**
+ * @author: renshuai
+ * @date: 2019/07/11 下午4:36
+ * @Description:
+ */
+public class NettyServer {
+}
