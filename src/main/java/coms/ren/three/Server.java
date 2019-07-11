613b4d73ce3642f74e646cd34ace786ebf2b9422
renshuai <renshuai@yuerushui.com>
2019-07-11 15:56:44 +0800

    init

---
+++ src/main/java/coms/ren/three/Server.java
@@ -0,0 +1,43 @@
+package coms.ren.three;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.AttributeKey;
+
+/**
+ * @author: renshuai
+ * @date: 2019/07/11 下午2:35
+ * @Description:
+ */
+public class Server {
+
+    public static void main(String[] args) throws InterruptedException {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childOption(ChannelOption.TCP_NODELAY, true)
+                    .childAttr(AttributeKey.newInstance("childAttr"), "childAttr")
+                    .handler(new ServerHandler())
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+
+                        }
+                    });
+            ChannelFuture channelFuture = b.bind(8888).sync();
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+}
